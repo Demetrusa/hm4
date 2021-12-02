f10894cde9e340a004bd5e19f316efd7865a2602
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2021-12-02 18:54:18 +0400

    third example

--- app.js
+++ app.js
@@ -30,9 +30,6 @@ const mainDiv = document.getElementById('main');
 const ul = document.createElement ('ul');
 mainDiv.appendChild(ul);
 
-
-
-
 arr.forEach(item => {
     const li = document.createElement ('li');
     li.textContent = item.age > 18 ? item.username : ""
@@ -43,3 +40,26 @@ arr.forEach(item => {
 //     let newArr = arr.filter(item => item.age > 18);
 //     return console.log(newArr)
 // }
+
+const buttonDiv = document.getElementById('thirdi');
+
+buttonDiv.style.width = '200px';
+buttonDiv.style.border =' 5px solid cyan';
+buttonDiv.style.textAlign = 'center';
+
+
+const btn = document.querySelector('#btn');
+// btn.style.width = "100px";
+btn.style.backgroundcolor = 'cyan';
+btn.setAttribute('type', 'submit');
+
+if (btn.getAttribute('type') === 'submit'){
+    btn.style.backgroundcolor = 'cyan';
+} else {
+    btn.getAttribute('disabled','');
+    btn.style.backgroundcolor = 'yellow';
+
+}
+
+
+
